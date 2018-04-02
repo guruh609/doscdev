8a18b4a4a3ea70856c4065c93fa9beee683c8bd4
rizkimufti <rizkimufti@gmail.com>
2018-04-02 22:45:59 +0700

    update sql

--- doscdev.sql
+++ doscdev.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 31, 2018 at 09:52 PM
+-- Generation Time: Apr 02, 2018 at 10:44 PM
 -- Server version: 5.7.20-0ubuntu0.16.04.1
 -- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `user`
+-- Database: `doscvdev`
 --
 
 -- --------------------------------------------------------
@@ -32,16 +32,6 @@ CREATE TABLE `massage` (
   `email` varchar(200) NOT NULL,
   `massage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `massage`
---
-
-INSERT INTO `massage` (`id_user`, `nama`, `email`, `massage`) VALUES
-(1, 'ONPHPID', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(2, 'Regha', 'member', 'aa08769cdcb26674c6706093503ff0a3'),
-(3, 'admin', 'admin', '0192023a7bbd73250516f069df18b500'),
-(4, 'aku', 'akukamu', 'a42d640365f9ba18208ab059cd068e09');
 
 -- --------------------------------------------------------
 
@@ -61,18 +51,27 @@ CREATE TABLE `pendaftar` (
 --
 
 INSERT INTO `pendaftar` (`id_user`, `nama`, `email`, `nohp`) VALUES
-(1, 'ONPHPID', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(2, 'Regha', 'member', 'aa08769cdcb26674c6706093503ff0a3'),
-(3, 'admin', 'admin', '0192023a7bbd73250516f069df18b500'),
-(4, 'aku', 'akukamu', 'a42d640365f9ba18208ab059cd068e09'),
-(5, 'dijah', 'dijah', 'fa13f6c9173b03b1709d9352f186515b'),
-(6, 'sdadjsh', 'sadhsjdhj', '123'),
-(7, 'asdjasdjjh', 'ashdjash', 'asdjasd'),
-(8, 'aku', 'ashdjash', 'asdjasd'),
-(9, 'aku', '', 'asdjasd'),
-(10, 'rizki', '', 'asdas'),
-(11, 'jojo', 'jojo2gmail,com', 'ini pesan kiu'),
-(12, 'aku', 'kamu@gmail.com', '09192919');
+(13, 'rizki', 'rizkimufti@gmail.com', '019209');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id_user` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `pesan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesan`
+--
+
+INSERT INTO `pesan` (`id_user`, `nama`, `email`, `pesan`) VALUES
+(18, 'Rizki ', 'rizkimufti@gmail.com', 'Mantap');
 
 -- --------------------------------------------------------
 
@@ -87,13 +86,6 @@ CREATE TABLE `users` (
   `password` text NOT NULL,
   `level_user` varchar(150) NOT NULL DEFAULT 'member'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id_user`, `nama`, `username`, `password`, `level_user`) VALUES
-(11, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'member');
 
 --
 -- Indexes for dumped tables
@@ -112,6 +104,12 @@ ALTER TABLE `pendaftar`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -125,17 +123,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `massage`
 --
 ALTER TABLE `massage`
-  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `pendaftar`
 --
 ALTER TABLE `pendaftar`
-  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
