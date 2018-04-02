9293b8d2d87aca944729118c4841be8fb983f739
rizkimufti <rizkimufti@gmail.com>
2018-04-02 20:16:09 +0700

    add login

---
+++ login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Login</title>
+
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="vendorblog/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="cssblog/blog-home.css" rel="stylesheet">
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">login</a>
+            </li>
+       
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="col-md-4 col-md-offset-4 form-login">
+    
+    <?php
+    /* handle error */
+    if (isset($_GET['error'])) : ?>
+        <div class="alert alert-warning alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <strong>Warning!</strong> <?=base64_decode($_GET['error']);?>
+        </div>
+    <?php endif;?>
+
+        <div class="outter-form-login">
+      
+            <form action="check-login.php" class="inner-login" method="post">
+            <h3 class="text-center title-login">Login Member</h3>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+                
+                <input type="submit" class="btn btn-block btn-custom-green" value="Login" />
+                
+                <div class="text-center forget">
+                    <p>Lupa Password ?</p>
+                </div>
+                <div class="text-center ">
+                    <a href="register.php">Belum Daftar ?</a>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
